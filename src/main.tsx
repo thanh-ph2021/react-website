735196bf30500c33081da2471e80e68255ef561d
thanh.ph2021 <thanh.ph2021@gmail.com>
2025-02-25 14:44:08 +0700

    fix bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 
 import App from './pages/App.tsx'
 import PrivacyPolicy from './pages/PrivacyPolicy.tsx'
 import TermsOfService from './pages/TermsOfService.tsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/react-website',
-    element: <App />
-  },
-  {
-    path: '/react-website/privacy-policy/:appId',
-    element: <PrivacyPolicy />
-  },
-  {
-    path: '/react-website/terms-of-service/:appId',
-    element: <TermsOfService />
-  }
-])
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/privacy-policy/:appId" element={<PrivacyPolicy />} />
+        <Route path="/terms-of-service/:appId" element={<TermsOfService />} />
+      </Routes>
+    </HashRouter>
+  </StrictMode>
+);
